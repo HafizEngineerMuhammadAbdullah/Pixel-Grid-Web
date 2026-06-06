f782782e85c0a561dec95412437e124d853114d2
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-06-06 15:18:20 -0700

    fixes heading animation

--- animation.js
+++ animation.js
@@ -1,35 +1,88 @@
 
-gsap.from('.header h1', {
-    y: -80,
+const splitText = () => {
+    const h1 = document.querySelector('.header h1');
+
+    const headingContent = h1.textContent;
+
+    let half = headingContent.length / 2;
+
+    let word = "";
+
+    headingContent.split("").forEach((char, idx) => {
+
+        if (idx < half) {
+            word += `<span class='left-half'>${char}</span>`;
+        } else {
+            word += `<span class='right-half'>${char}</span>`;
+        }
+    });
+
+    h1.innerHTML = word;
+}
+
+splitText();
+
+gsap.from('h1 span.left-half', {
+    y: 200,
+    duration: 1,
+    delay: 0.5,
+    stagger: 0.3,
+    opacity: 0
+});
+
+gsap.from('h1 span.right-half', {
+    y: 200,
+    duration: 1,
+    delay: 0.5,
+    stagger: -0.3,
+    opacity: 0
+});
+
+// gsap.from('.header h1', {
+//     y: -80,
+//     x: 100,
+//     opacity: 0,
+//     duration: 2
+// })
+
+gsap.from('.header p', {
+    y: -100,
     x: 100,
     opacity: 0,
     duration: 2
 })
-
-gsap.from('.header p', {
+gsap.from('.header button', {
     x: -250,
     opacity: 0,
     duration: 2
 })
-gsap.from('.header button', {
-    scale: 0,
-    opacity: 0,
-    rotation: 720,
-    duration: 2,
-})
+// gsap.from('.header button', {
+//     scale: 0,
+//     opacity: 0,
+//     rotation: 720,
+//     duration: 2,
+// })
+
+
+gsap.registerPlugin(ScrollTrigger);
 gsap.utils.toArray(".image-card").forEach(card => {
     gsap.from(card, {
-        y: 100,
         scale: 0,
         opacity: 0,
         rotation: 360,
-        duration: 3,
+        duration: 2.5,
+        // y: 100,
+        // scale: 0.8,
+        // opacity: 0,
+        // duration: 1,
+        ease: "power3.out",
+        delay: 0.5,
         scrollTrigger: {
-            trigger: ".image-card",
+            trigger: card,
             scroller: "body",
             markers: false,
             toggleActions: "play none none reverse",
-            start: "top 5%",
+            start: "top 80%",
             pin: false,
         }
     })
